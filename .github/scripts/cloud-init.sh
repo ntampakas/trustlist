52af077c2d8f1666a90ca45df96bf5657775c9b6
Nick <nick@gmx.co.uk>
2023-10-24 10:01:28 +0300

    Add workflow to spawn an ephemeral instance

---
+++ .github/scripts/cloud-init.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eux
+trap 'poweroff' TERM EXIT INT
+
+Repo=$(curl http://169.254.169.254/latest/meta-data/tags/instance/Repo)
+Branch=$(curl http://169.254.169.254/latest/meta-data/tags/instance/Branch)
+
+Local_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+# Install pkgs
+apt-get update
+apt-get install -y net-tools
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker ubuntu
+
+su -P ubuntu -c "git clone -b $Branch --single-branch https://github.com/ntampakas/$Repo.git /home/ubuntu/$Repo"
+su -P ubuntu -c "sed -i \"s/http:\/\/127.0.0.1:8000/http:\/\/$Local_IP:8000/g\" /home/ubuntu/$Repo/packages/frontend/src/config.ts"
+su -P ubuntu -c "sed -i \"s/node-ip/$Local_IP/\" /home/ubuntu/$Repo/compose.yaml"
+su -P ubuntu -c "cd /home/ubuntu/$Repo ; docker compose up -d"
+
+sleep 12h
---
+++ .github/scripts/run.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -eux
+
+TRIGGER=$1
+BRANCH=$2
+ACTOR=$3
+SHA=$4
+AMI="ami-04e601abe3e1a910f"
+SG="sg-03c0a0de6836d583d"
+SUBNET="subnet-07ce3c81e409f4e14"
+VPC="vpc-05dedcb650bd24f8d"
+
+# Kill old instances
+CURRENT_TIME_EPOCH=$(date -d `date -Is` +"%s")
+EC2=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=[running]" "Name=tag:Name,Values='Drill-*'" "Name=network-interface.vpc-id,Values=[$VPC]" --query "Reservations[*].Instances[*].InstanceId" --output text)
+for i in $EC2; do
+  EC2_LAUNCH_TIME=$(aws ec2 describe-instances --instance-ids $i --query 'Reservations[*].Instances[*].LaunchTime' --output text)
+  LAUNCH_TIME_EPOCH=$(date -d $EC2_LAUNCH_TIME +"%s")
+  diff=$(expr $CURRENT_TIME_EPOCH - $LAUNCH_TIME_EPOCH)
+
+  #if [ $diff -gt 43200 ]; then
+  if [ $diff -gt 1000 ]; then
+    aws ec2 terminate-instances --instance-ids $i
+  fi
+done
+
+# Stop here in schedule
+[ ! $TRIGGER = "maintenance" ] || exit 0
+
+# Check if actor ec2 exists
+ACTOR_EC2=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=[running]" "Name=tag:Name,Values='Drill-$ACTOR-*'" "Name=network-interface.vpc-id,Values=[$VPC]" --query "Reservations[*].Instances[*].InstanceId" --output text)
+
+[ -z $ACTOR_EC2 ] || aws ec2 terminate-instances --instance-ids $ACTOR_EC2
+
+# Launch new instance
+aws ec2 run-instances \
+  --user-data "file://.github/scripts/cloud-init.sh" \
+  --image-id $AMI \
+  --count 1 \
+  --instance-type t3.xlarge \
+  --key-name devops \
+  --security-group-ids $SG \
+  --subnet-id $SUBNET \
+  --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"VolumeSize\":20,\"DeleteOnTermination\":true}}]" \
+  --instance-initiated-shutdown-behavior terminate \
+  --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value="Drill-$ACTOR-$SHA"},{Key=Repo,Value="trustlist"},{Key=Branch,Value="$BRANCH"}]" \
+  --metadata-options "InstanceMetadataTags=enabled"
