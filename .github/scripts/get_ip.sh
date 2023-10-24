7a71691e6c98174db61e57b6a95d2acb59a6083f
Nick <nick@gmx.co.uk>
2023-10-24 15:33:56 +0300

    Add sleep

--- .github/scripts/get_ip.sh
+++ .github/scripts/get_ip.sh
@@ -5,6 +5,8 @@ set -eux
 ACTOR=$1
 VPC="vpc-04848f977fbc9560d"
 
+sleep 10
+
 EC2_IP=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=[running]" "Name=tag:Name,Values='trustlist-ephemeral-$ACTOR-*'" "Name=network-interface.vpc-id,Values=[$VPC]" --query "Reservations[*].Instances[*].[NetworkInterfaces[*].[PrivateIpAddress]]" --output text)
 
 echo "URL: http://$EC2_IP:4000"
